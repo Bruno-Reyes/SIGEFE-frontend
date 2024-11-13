3747a12a180c8f7d4fa7e32ff0cbd78e1a2f7abc
JorgeOwl <jhernandez.owl@gmail.com>
2024-11-13 17:34:07 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';          
 import 'primeicons/primeicons.css';                        
 import 'primeflex/primeflex.css';    
-import './index.css'
+//import './index.css'
 
 import App from './App.jsx'
 
